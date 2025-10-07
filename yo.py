c4e4834fcadad60d2c9e92a952949c179da79875
lovisa <BUFFlovisa>
2025-10-07 10:22:44 +0200

    heh

--- yo.py
+++ yo.py
@@ -1,6 +1,8 @@
 import math 
 
-for i in range(1, 10):          
-    for j in range(1,+ 10):       
-        print(f"{i*j}", end=' ')
-    print("  ")                   
+tal = int(input(""))
+
+for i in range(1,tal + 1):          
+    for j in range(1, tal + 1):       
+        print(f"{i*j:3}", end=' ')
+    print("")  
